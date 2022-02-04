427bbc098f020e64c941de9746a2ad2ceaf4301c
pmoeini1 <pmoeini@uwo.ca>
2022-02-04 15:15:57 -0500

    2nd part of level 2 added

--- saeg/src/components/levels/LevelTwo.js
+++ saeg/src/components/levels/LevelTwo.js
@@ -10,9 +10,14 @@ export default class LevelTwo extends Component {
             counter: 0,
             message: 'Click or drag onto the line the 5 elements on the left',
             arrayTwo: null,
+            arrayThree: null,
             nums: [],
             stepOneLeft: [],
             stepOneRight: [],
+            stepTwoLeft1: [],
+            stepTwoLeft2: [],
+            stepTwoRight1: [],
+            stepTwoRight2: [],
         }
         for (let i=0; i<10; i++) {
             // add 10 random integers between 1 and 20 to nums array
@@ -20,6 +25,7 @@ export default class LevelTwo extends Component {
         }
     }
     render() {
+        //TODO: add audio feedback where we have alerts
         const arrayOne = [...this.state.nums].map((value, index) => {
             return (
                 <Draggable>
@@ -63,12 +69,48 @@ export default class LevelTwo extends Component {
                                             <Draggable>
                                                 <Button key={index} 
                                                     style={{
-                                                        marginTop: "10%",
+                                                        marginTop: "5%",
                                                     }}
                                                     variant="secondary"
                                                     onClick={
                                                         () => {
-                                                            // add stuff for 2nd level left
+                                                            if (this.state.counter===2) {
+                                                                this.state.stepTwoLeft1.push(value);
+                                                                if (this.state.stepTwoLeft1.length>=3) {
+                                                                    let temp = [...this.state.nums].slice(0, 3);
+                                                                    for (let i=0; i<3; i++) {
+                                                                        if (!temp.includes(this.state.stepTwoLeft1[i])) {
+                                                                            alert("wrong");
+                                                                            window.location.reload();
+                                                                        } else {
+                                                                            temp.splice(temp.indexOf(this.state.stepTwoLeft1[i]), 1);
+                                                                        }
+                                                                    }
+                                                                    alert("correct");
+                                                                    this.setState({
+                                                                        counter: 3,
+                                                                        message:  'Click or drag the 2 elements on the right half of the left array',
+                                                                    });
+                                                                }
+                                                            } else if (this.state.counter===3) {
+                                                                this.state.stepTwoLeft2.push(value);
+                                                                if (this.state.stepTwoLeft2.length>=2) {
+                                                                    let temp = [...this.state.nums].slice(3, 5);
+                                                                    for (let i=0; i<2; i++) {
+                                                                        if (!temp.includes(this.state.stepTwoLeft2[i])) {
+                                                                            alert("wrong");
+                                                                            window.location.reload();
+                                                                        } else {
+                                                                            temp.splice(temp.indexOf(this.state.stepTwoLeft2[i]), 1);
+                                                                        }
+                                                                    }
+                                                                    alert("correct");
+                                                                    this.setState({
+                                                                        counter: 4,
+                                                                        message:  'Click or drag the 3 elements on the left half of the right array',
+                                                                    });
+                                                                }
+                                                            }
                                                         }
                                                     }
                                                 >
@@ -83,11 +125,132 @@ export default class LevelTwo extends Component {
                                                 <Button key={index}
                                                     variant="secondary"
                                                     style={{
-                                                        marginTop: "10%",
+                                                        marginTop: "5%",
                                                     }}
                                                     onClick={
                                                         () => {
-                                                            // add stuff for 2nd level
+                                                            if (this.state.counter===4) {
+                                                                this.state.stepTwoRight1.push(value);
+                                                                if (this.state.stepTwoRight1.length>=3) {
+                                                                    let temp = [...this.state.nums].slice(5, 8);
+                                                                    for (let i=0; i<3; i++) {
+                                                                        if (!temp.includes(this.state.stepTwoRight1[i])) {
+                                                                            alert("wrong");
+                                                                            window.location.reload();
+                                                                        } else {
+                                                                            temp.splice(temp.indexOf(this.state.stepTwoRight1[i]), 1);
+                                                                        }
+                                                                    }
+                                                                    alert("correct");
+                                                                    this.setState({
+                                                                        counter: 5,
+                                                                        message:  'Click or drag the 2 elements on the right half of the right array',
+                                                                    });
+                                                                }
+                                                            } else if (this.state.counter===5) {
+                                                                this.state.stepTwoRight2.push(value);
+                                                                if (this.state.stepTwoRight2.length>=2) {
+                                                                    let temp = [...this.state.nums].slice(8, 10);
+                                                                    for (let i=0; i<2; i++) {
+                                                                        if (!temp.includes(this.state.stepTwoRight2[i])) {
+                                                                            alert("wrong");
+                                                                            window.location.reload();
+                                                                        } else {
+                                                                            temp.splice(temp.indexOf(this.state.stepTwoRight2[i]), 1);
+                                                                        }
+                                                                    }
+                                                                    alert("correct");
+                                                                    // TODO: set up the onClick for all button maps included in arrayThree
+                                                                    const array3Left1 = [...this.state.nums].slice(0, 3).map((value) => {
+                                                                        return (
+                                                                            <Draggable>
+                                                                                <Button key={index}
+                                                                                    variant="success"
+                                                                                    style={{
+                                                                                        marginTop: "5%",
+                                                                                    }}
+                                                                                    onClick={
+                                                                                        () => {
+
+                                                                                        }
+                                                                                    }
+                                                                                    >
+                                                                                    {value}
+                                                                                </Button>
+                                                                            </Draggable>
+                                                                        )
+                                                                    });
+                                                                    const array3Left2 = [...this.state.nums].slice(3, 5).map((value) => {
+                                                                        return (
+                                                                            <Draggable>
+                                                                                <Button key={index}
+                                                                                    variant="success"
+                                                                                    style={{
+                                                                                        marginTop: "5%",
+                                                                                    }}
+                                                                                    onClick={
+                                                                                        () => {
+                                                                                            
+                                                                                        }
+                                                                                    }
+                                                                                    >
+                                                                                    {value}
+                                                                                </Button>
+                                                                            </Draggable>
+                                                                        )
+                                                                    });
+                                                                    const array3Right1 = [...this.state.nums].slice(5, 8).map((value) => {
+                                                                        return (
+                                                                            <Draggable>
+                                                                                <Button key={index}
+                                                                                    variant="success"
+                                                                                    style={{
+                                                                                        marginTop: "5%",
+                                                                                    }}
+                                                                                    onClick={
+                                                                                        () => {
+                                                                                            
+                                                                                        }
+                                                                                    }
+                                                                                    >
+                                                                                    {value}
+                                                                                </Button>
+                                                                            </Draggable>
+                                                                        )
+                                                                    });
+                                                                    const array3Right2 = [...this.state.nums].slice(8, 10).map((value) => {
+                                                                        return (
+                                                                            <Draggable>
+                                                                                <Button key={index}
+                                                                                    variant="success"
+                                                                                    style={{
+                                                                                        marginTop: "5%",
+                                                                                    }}
+                                                                                    onClick={
+                                                                                        () => {
+                                                                                            
+                                                                                        }
+                                                                                    }
+                                                                                    >
+                                                                                    {value}
+                                                                                </Button>
+                                                                            </Draggable>
+                                                                        )
+                                                                    });
+                                                                    const arrayThree = (
+                                                                        <div>
+                                                                            {array3Left1} {array3Left2} {array3Right1} {array3Right2}
+                                                                            {/* TODO: fix the following lines */}
+                                                                            <h3>_________ ______ _________ ______</h3>
+                                                                        </div>
+                                                                    );
+                                                                    this.setState({
+                                                                        counter: 6,
+                                                                        message:  'Click or drag the 2 leftmost elements on the 1st subarray from the left in the 3rd row',
+                                                                        arrayThree: arrayThree,
+                                                                    });
+                                                                }
+                                                            }
                                                         }
                                                     }
                                                 >
@@ -99,6 +262,8 @@ export default class LevelTwo extends Component {
                                     const arrayTwo = (
                                         <div>
                                             {array2Left} {array2Right}
+                                            {/* TODO: fix the format of the lines below */}
+                                            <h3>_________ ______ _________ ______</h3>
                                         </div>
                                     );
                                     this.setState({
@@ -114,13 +279,20 @@ export default class LevelTwo extends Component {
             )
         });
         return (
-            <div>
+            <div style={{
+                marginLeft: "20%",
+            }}
+            >
                 <h1>Level One</h1>
                 <h2>MergeSort Algorithm</h2>
+                <h4>Ensure NOT to double click buttons</h4>
+                <h4>Perform each step left-to-right</h4>
+                <br />
                 <h3>{this.state.message}</h3>
                 {arrayOne}
-                <h3>________________    ________________</h3>
+                <h3>________________ __________________</h3>
                 {this.state.arrayTwo}
+                {this.state.arrayThree}
             </div>
         )
     }
