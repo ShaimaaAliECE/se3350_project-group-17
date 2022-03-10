ea7a6676cdc9d2fe4020448e38dcebafc9bf9eb0
taralaird <tlaird4@uwo.ca>
2022-03-10 16:34:17 -0500

    Create lifeSystem.js

---
+++ saeg/src/functions/lifeSystem/lifeSystem.js
@@ -0,0 +1,4 @@
+/*int used to count number of lives, start at 3 and decrement for each wrong,
+ display this number during the entiry of the level, 
+ when 0 is reacehed allow  user to change to new level, restart level, or quit gane 
+ */
