e188d4af1c382bfeeb2e6ad530604ffe4d797a50
Varan Gill <gillzx01@gmail.com>
2022-03-24 13:22:40 -0400

    Finish MergeSorting for Custom Level

--- saeg/src/components/levels/Level.js
+++ saeg/src/components/levels/Level.js
@@ -15,17 +15,31 @@ export default class Level extends Component {
 
     componentDidMount() {
         let arrayToFill = [];
-        let answersStorage = [];
+        let answersStorage = []; //first half of the algorithm - 
+        let answersStorage2 = [];
         let newInt = 0;
         for (let i = 0; i < this.props.len; i++) { //creating the randomized array
             newInt = Math.floor(Math.random() * (this.props.max - this.props.min + 1) + parseInt(this.props.min));
             arrayToFill.push(newInt);
         }
-        mergeSort(arrayToFill, answersStorage);
+        let sorted = mergeSort(arrayToFill, answersStorage, answersStorage2);
+
+        let singlesArray = []; //array to keep track of 
+        for (let i = 0; i < answersStorage.length; i++) { //loop through once to find arrays with only 1 value
+            if (answersStorage[i].length == 1) {
+                singlesArray.push(answersStorage[i][0]);
+            }
+        }
+        answersStorage.push([...singlesArray]); //so that when merging the subarrays, users can 
+        for (let i = 0; i < answersStorage2.length; i++) {
+            answersStorage.push([...answersStorage2[i]]);
+        }
+        answersStorage.push([...sorted]); //push the final sorted array
+        console.log(answersStorage);
 
         //creating the grid inputs array
         let emptyArray = Array(answersStorage.length).fill(null);
-        let tempArray = [];
+        let tempArray = []; //temporary array to keep track of 
         emptyArray[0] = [ ...answersStorage[0]]; //first array will be filled automatically
         for (let i = 1; i < answersStorage.length; i++) {
             for (let j = 0; j < answersStorage[i].length; j++) {
@@ -52,18 +66,51 @@ export default class Level extends Component {
                     temp[indexes[0]][indexes[1]] = val;
                     return {
                         ...prevState,
-                        inputted: [...prevState.inputted, key],
                         inputs: [...temp]
                     }
                 }, () => {
                     //console.log("inputs below")
                     //console.log(this.state.inputs)
                     if (val === this.state.answers[this.state.currentPos[0]][this.state.currentPos[1]]) { //correct input
-                        console.log("correct")
+                        console.log("correct");
+                        this.setState((prevState) => {
+                            let temp2 = [...prevState.inputs];
+                            temp2[this.state.currentPos[0]][this.state.currentPos[1]] = val;
+                            let tempPos = [];
+                            console.log("next:" + this.state.answers[this.state.currentPos[0]][this.state.currentPos[1]+1]);
+                            if (this.state.answers[this.state.currentPos[0]][this.state.currentPos[1]+1]) {
+                                tempPos = [...this.state.currentPos];
+                                tempPos[1] += 1;
+                            } else {
+                                console.log("new row");
+                                let nextRow = this.state.currentPos[0] + 1;
+                                tempPos = [...this.state.currentPos];
+                                tempPos[1] = 0;
+
+                                if (this.state.answers[nextRow].length == this.state.answers[0].length && this.state.answers[nextRow+1]) {
+                                    console.log("midpoint");
+                                    temp2[nextRow] = this.state.answers[nextRow];
+                                    tempPos[0] += 2;
+                                } else {
+                                    tempPos[0] += 1;
+                                }
+                            }
+                            return {
+                                ...prevState,
+                                inputs: [...temp2],
+                                currentPos: [...tempPos],
+                                inputted: [...prevState.inputted, key]
+                            };
+                        }, () => {
+                            console.log(this.state.inputs);
+                        });
+
+
                     } else { //incorrect input
                         console.log("incorrect");
                         console.log("guess value: " + val)
                         console.log("answer: " + this.state.answers[this.state.currentPos[0]][this.state.currentPos[1]]);
+                        console.log(this.state.answers);
                     }
                 });
             } else { //if the tile was already clicked
--- saeg/src/functions/MergeSort.js
+++ saeg/src/functions/MergeSort.js
@@ -1,5 +1,13 @@
-export function merge(left, right) {
+export function merge(left, right, storeResults2) {
     let arr = []
+
+    if (left.length > 1) {
+        storeResults2.push([...left]);
+    }
+    if (right.length > 1) {
+        storeResults2.push([...right]);
+    }
+
     while (left.length && right.length) {
         if (left[0] < right[0]) {
             arr.push(left.shift())  
@@ -13,7 +21,7 @@ export function merge(left, right) {
     return [ ...arr, ...left, ...right ]
 }
 
-export function mergeSort(array, storeResults) {
+export function mergeSort(array, storeResults, storeResults2) {
     const half = array.length / 2
     storeResults.push([ ...array]);
 
@@ -22,5 +30,5 @@ export function mergeSort(array, storeResults) {
     }
     
     const left = array.splice(0, half)
-    return merge(mergeSort(left, storeResults),mergeSort(array, storeResults));
+    return merge(mergeSort(left, storeResults, storeResults2),mergeSort(array, storeResults, storeResults2), storeResults2);
 }
