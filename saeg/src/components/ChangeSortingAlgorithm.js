393baaec5acc151656b1140831901f45060dba8b
ohoward2 <ohoward2@uwo.ca>
2022-03-24 23:13:36 -0400

    Page Descriptions
    Added page descriptions/instructions on the Level Select page and the Select algorithm page.
    
    Co-Authored-By: ltidy <71096536+ltidy@users.noreply.github.com>

--- saeg/src/components/ChangeSortingAlgorithm.js
+++ saeg/src/components/ChangeSortingAlgorithm.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, ButtonGroup, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
-
-export default class LevelSelect extends Component {
+//Select Sorting Algorithm Page
+export default class ChangeSortingAlgorithm extends Component {
 
     render() {
         return (
+            //Header, description, radio buttons, and "Return to Level Select Button" for Select Sorting Algorithm page
             <div style={{marginLeft: "20%", marginTop: "2%"}}>
                 <h1>Select Sorting Algorithm</h1>
-            <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                <p1>Select the type of sorting algorithm by using the radio buttons below.</p1>
+                <br></br>
+                <br></br>
+                <p2>(This feature is currently not functional as we are not implementing other 
+                    algorithms in this project. 
+                    <br></br>
+                    However, if we were to update the game with more 
+                    sorting algorithms, this is where you would select the type of sorting algorithm)</p2>
+
+            <ToggleButtonGroup type="radio" name="options" defaultValue={1} style={{marginTop: "2%"}}>
                 <ToggleButton id="tbg-radio-1" value={1} variant='outline-success'>Merge Sort</ToggleButton>
                 <ToggleButton id="tbg-radio-2" value={2} variant='outline-success'>Quick Sort</ToggleButton>
                 <ToggleButton id="tbg-radio-3" value={3} variant='outline-success'>Bubble Sort</ToggleButton>
